895dd343823ab22e342762e87e77579bac3c2d4d
tsalmon <th_s@hotmail.fr>
2015-03-16 14:41:44 +0100

    --/*.../

--- src/Valgen.java
+++ src/Valgen.java
@@ -1,50 +1,17 @@
 import java.util.* ;
-
-/*
- * An ValgenState is a node with a set of outgoing edges to other
- * NFAStates.
- *
- * There are two kinds of edges:
- *
- * (1) Empty edges allow the Valgen to transition to that state without
- *     consuming a character of input.
- * 
- * (2) Character-labelled edges allow the Valgen to transition to that
- *     state only by consuming the character on the label.
- *
- */
 class ValgenState
 {
-    /*
-     * WARNING:
-     *
-     * The maximum integer character code we'll match is 255, which
-     * is sufficient for the ASCII character set.
-     *
-     * If we were to use this on the Unicode character set, we'd get
-     * an array index out-of-bounds exception.
-     *
-     * A ``proper'' implementation of this would not use arrays but
-     * rather a dynamic data structure like Vector.
-     */
+
     public static final int MAX_CHAR = 255 ;
 
 	    public boolean isFinal               = false ;
     private ArrayList<ValgenState> onChar[] = new ArrayList[MAX_CHAR] ;
     private ArrayList<ValgenState> onEmpty  = new ArrayList() ;
 
-    /*
-     * Add a transition edge from this state to next which consumes
-     * the character c.
-     */
     public void addCharEdge(char c, ValgenState next) {
 		onChar[(int)c].add(next) ;
     }
 
-    /*
-     * Add a transition edge from this state to next that does not
-     * consume a character.
-     */
     public void addEmptyEdge(ValgenState next) {
 		onEmpty.add(next) ;
     }
@@ -59,68 +26,31 @@ class ValgenState
     }
 
     private boolean matches(String s, ArrayList visited) {
-		/*
-		 * When matching, we work character by character.
-		 *
-		 * If we're out of characters in the string, we'll check to
-		 * see if this state if final, or if we can get to a final
-		 * state from here through empty edges.
-		 *
-		 * If we're not out of characters, we'll try to consume a
-		 * character and then match what's left of the string.
-		 *
-		 * If that fails, we'll ask if empty-edge neighbors can match
-		 * the entire string.
-		 *
-		 * If that fails, the match fails.
-		 *
-		 * Note: Because we could have a circular loop of empty
-		 * transitions, we'll have to keep track of the states we
-		 * visited through empty transitions so we don't end up
-		 * looping forever.
-		 */
 
 		if (visited.contains(this)) 
-		    /* We've found a path back to ourself through empty edges;
-		     * stop or we'll go into an infinite loop. */
+
 		    return false ;
-		
-		/* In case we make an empty transition, we need to add this
-		 * state to the visited list. */
+
 		visited.add(this) ;
 
 		if (s.length() == 0) {
-		    /* The string is empty, so we match this string only if
-		     * this state is a final state, or we can reach a final
-		     * state without consuming any input. */
-		    if (isFinal)
-			return true ;
 
-		    /* Since this state is not final, we'll ask if any
-		     * neighboring states that we can reach on empty edges can
-		     * match the empty string. */
+		    if (isFinal)
+				return true ;
+
 		    for (ValgenState next : onEmpty) {
 			if (next.matches("",visited))
 			    return true ;
 		    }
 		    return false ;
 		} else {
-		    /* In this case, the string is not empty, so we'll pull
-		     * the first character off and check to see if our
-		     * neighbors for that character can match the remainder of
-		     * the string. */
-
 		    int c = (int)s.charAt(0) ;
 
 		    for (ValgenState next : onChar[c]) {
-			if (next.matches(s.substring(1)))
-			    return true ;
+				if (next.matches(s.substring(1)))
+				    return true ;
 		    }
 
-		    /* It looks like we weren't able to match the string by
-		     * consuming a character, so we'll ask our
-		     * empty-transition neighbors if they can match the entire
-		     * string. */
 		    for (ValgenState next : onEmpty) {
 			if (next.matches(s,visited))
 			    return true ;
@@ -130,14 +60,6 @@ class ValgenState
     }
 }
 
-/*
- * Here, an Valgen is represented by an entry state and an exit state.
- *
- * Any Valgen can be represented by an Valgen with a single exit state by
- * creating a special exit state, and then adding empty transitions
- * from all final states to the special one.
- *
- */
 public class Valgen
 {
     public ValgenState entry ;
@@ -152,9 +74,6 @@ public class Valgen
 	return entry.matches(str);
     }
 
-    /*
-     * c() : Creates an Valgen which just matches the character `c'.
-     */
     public static final Valgen c(char c) {
 	ValgenState entry = new ValgenState() ;
 	ValgenState exit = new ValgenState() ;
@@ -163,9 +82,6 @@ public class Valgen
 	return new Valgen(entry,exit) ;
     }
 
-    /*
-     * e() : Creates an Valgen which matches the empty string.
-     */
     public static final Valgen e() {
 	ValgenState entry  = new ValgenState() ;
 	ValgenState exit = new ValgenState() ;
@@ -174,20 +90,20 @@ public class Valgen
 	return new Valgen(entry,exit) ;
     }
 
-    public static final Valgen rep(Valgen nfa) {
+    public static final Valgen rep(Object nfa) {
 	nfa.exit.addEmptyEdge(nfa.entry) ;
         nfa.entry.addEmptyEdge(nfa.exit) ;
 	return nfa ;	
     }
 
-    public static final Valgen g(Valgen first, Valgen second) {
+    public static final Valgen g(Object first, Valgen second) {
 	first.exit.isFinal = false ;
 	second.exit.isFinal = true ;
-	first.exit.addEmptyEdge(second.entry) ;
+	first.exit.addEmptyEdge(second.entirey) ;
 	return new Valgen(first.entry,second.exit) ;
     }
 
-    public static final Valgen or(Valgen choice1, Valgen choice2) {
+    public static final Valgen or(Object choice1, Object choice2) {
 		choice1.exit.isFinal = false ;
 		choice2.exit.isFinal = false ;
 		ValgenState entry = new ValgenState() ;
@@ -200,7 +116,6 @@ public class Valgen
 		return new Valgen(entry,exit) ;
 	    }
 
-	    /* Syntactic sugar. */
 	    public static final Valgen re(Object o) {
 		if (o instanceof Valgen)
 		    return (Valgen)o ;
