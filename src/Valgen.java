e06f841dab0ee9a9bedf47579eb5fedfbb4f187c
tsalmon <th_s@hotmail.fr>
2015-03-18 18:00:06 +0100

    add comments

--- src/Valgen.java
+++ src/Valgen.java
@@ -1,166 +1,285 @@
 import java.util.* ;
-class ValgenState
-{
 
-    public static final int MAX_CHAR = 255 ;
+/**
+ * 
+ * Cette classe gere les transition et l'état d'un noeud de l'automate Valgen
+ * @author salmon
+ * @see Valgen
+ */
+class ValgenNode{
 
-	    public boolean isFinal               = false ;
-    private ArrayList<ValgenState> onChar[] = new ArrayList[MAX_CHAR] ;
-    private ArrayList<ValgenState> onEmpty  = new ArrayList() ;
+	public static final int MAX_CHAR = 255 ;
 
-    public void addCharEdge(char c, ValgenState next) {
-		onChar[(int)c].add(next) ;
-    }
+	public boolean isFinal                 = false ;
+	@SuppressWarnings("unchecked")
+	private ArrayList<ValgenNode>[] onChar = new ArrayList[MAX_CHAR] ;
+	private ArrayList<ValgenNode> onEmpty  = new ArrayList<ValgenNode>() ;
 
-    public void addEmptyEdge(ValgenState next) {
+	/**
+	 * Pour une transition c on ajoute un noeud
+	 * @param c la valeur de la transition
+	 * @param next le noeud a ajouter
+	 * @see ArrayList#add(Object) add 
+	 */
+	public void addCharEdge(char c, ValgenNode next) {
+		onChar[c].add(next) ;
+	}
+
+	/**
+	 * Relier le noeud sur l'objet par une epsilon-transition
+	 * @param next noeud a relier
+	 */
+	public void addEmptyEdge(ValgenNode next) {
 		onEmpty.add(next) ;
-    }
+	}
 
-    public ValgenState () {
-	for (int i = 0; i < onChar.length; i++)
-	    onChar[i] = new ArrayList() ;
-    }
+	public ValgenNode () {
+		for (int i = 0; i < onChar.length; i++)
+			onChar[i] = new ArrayList<ValgenNode>() ;
+	}
 
-    public boolean matches(String s) {
-		return matches(s,new ArrayList()) ;
-    }
+	/**
+	 * On appel ValgenNode.matches(String, ArrayList), 
+	 * en initialisant visited par un ArrayList vide
+	 * @param s Chaine a verifier
+	 * @return reponse
+	 * @see ValgenNode#matches(String, ArrayList) matches
+	 */
+	public boolean matches(String s) {
+		return matches(s,new ArrayList<ValgenNode>()) ;
+	}
 
-    private boolean matches(String s, ArrayList visited) {
-
-		if (visited.contains(this)) 
-
-		    return false ;
-
+	/**
+	 * Parcourir les voisins d'un noeud pour verifier qu'une chaine corr
+	 * @param s Chaine a verifier
+	 * @param visited Liste des noeuds deja parcours
+	 * @return reponse
+	 */
+	private boolean matches(String s, ArrayList<ValgenNode> visited) {
+		if (visited.contains(this)) //si on est deja passé sur le noeud, on l'ignore
+			return false;
 		visited.add(this) ;
-
 		if (s.length() == 0) {
-
-		    if (isFinal)
+			if (isFinal)
 				return true ;
-
-		    for (ValgenState next : onEmpty) {
-			if (next.matches("",visited))
-			    return true ;
-		    }
-		    return false ;
-		} else {
-		    int c = (int)s.charAt(0) ;
-
-		    for (ValgenState next : onChar[c]) {
+			for (ValgenNode next : onEmpty) {
+				if (next.matches("",visited))
+					return true ;
+			}
+			return false ;
+		} else { //recursion tant que s non vide, on lui retire la premiere lettre a chaque fois qu'on match
+			int c = (int)s.charAt(0) ;
+			for (ValgenNode next : onChar[c]) { // transition avec valeur
 				if (next.matches(s.substring(1)))
-				    return true ;
-		    }
-
-		    for (ValgenState next : onEmpty) {
-			if (next.matches(s,visited))
-			    return true ;
-		    }
-		    return false ;
+					return true ;
+			}
+			for (ValgenNode next : onEmpty) { // epsilon-transition
+				if (next.matches(s,visited))
+					return true ;
+			}
+			return false ;
 		}
-    }
+	}
 }
 
-public class Valgen
-{
-    public ValgenState entry ;
-    public ValgenState exit ;
+/**
+ * 
+ * @author salmon
+ *
+ */
+public class Valgen{
+	public ValgenNode entry ;
+	public ValgenNode exit ;
 
-    public Valgen(ValgenState entry, ValgenState exit) {
-	this.entry = entry ;
-	this.exit  = exit;
-    } 
+	public Valgen(ValgenNode entry, ValgenNode exit) {
+		this.entry = entry ;
+		this.exit  = exit;
+	} 
 
-    public boolean matches(String str) {
-	return entry.matches(str);
-    }
+	/**
+	 * Verifier qu'une chaine de caratere peut etre reconnue par une expression reguliere , 
+	 * On se contente d'appeler le point d'entrée de l'automate, et d'itéré de voisins en voisins
+	 *  en respectant les caracteres de la chaine
+	 * c'est a dire pouvant atteindre un etat final de l'automate valgen
+	 * @param str Chaine a verifier
+	 * @return reponse booleenne
+	 * @see ValgenNode#matches(String) matches
+	 */
+	public boolean matches(String str) {
+		return entry.matches(str);
+	}
 
-    public static final Valgen c(char c) {
-	ValgenState entry = new ValgenState() ;
-	ValgenState exit = new ValgenState() ;
-	exit.isFinal = true ;
-	entry.addCharEdge(c,exit) ;
-	return new Valgen(entry,exit) ;
-    }
+	/**
+	 * Affecte un caractere sur une transition entre deux noeuds
+	 * @param c caractere
+	 * @return la transition avec le caractere
+	 */
+	public static final Valgen c(char c) {
+		System.out.println("c(" + c + ")");
+		ValgenNode entry = new ValgenNode() ;
+		ValgenNode exit = new ValgenNode() ;
+		exit.isFinal = true ;
+		entry.addCharEdge(c,exit) ;
+		return new Valgen(entry,exit) ;
+	}
 
-    public static final Valgen e() {
-	ValgenState entry  = new ValgenState() ;
-	ValgenState exit = new ValgenState() ;
-	entry.addEmptyEdge(exit) ;
-	exit.isFinal = true ;
-	return new Valgen(entry,exit) ;
-    }
+	/**
+	 * On fabrique un automate simple, constitué de 2 noeuds, relié par une epsilon-transition
+	 * @return un automate Valgen
+	 */
+	private static final Valgen e() {
+		ValgenNode entry  = new ValgenNode() ;
+		ValgenNode exit = new ValgenNode() ;
+		entry.addEmptyEdge(exit) ;
+		exit.isFinal = true ;
+		return new Valgen(entry,exit) ;
+	}
 
-    public static final Valgen r(Valgen nfa) {
-	nfa.exit.addEmptyEdge(nfa.entry) ;
-        nfa.entry.addEmptyEdge(nfa.exit) ;
-	return nfa ;	
-    }
+	/**
+	 * Concatenation de deux automates, on prend la sortie du premier et on la relie au second.
+	 * La sortie du 1ere automate n'est plus un etat final
+	 * @param first automate Valgne
+	 * @param second un autre automate Valgen
+	 * @return un automate Valgen
+	 */
+	private static final Valgen concat(Valgen first, Valgen second) {
+		System.out.println("s");
+		first.exit.isFinal = false ;
+		first.exit.addEmptyEdge(second.entry) ;
+		return new Valgen(first.entry,second.exit) ;
+	}
 
-    public static final Valgen s(Valgen first, Valgen second) {
-	first.exit.isFinal = false ;
-	second.exit.isFinal = true ;
-	first.exit.addEmptyEdge(second.entry) ;
-	return new Valgen(first.entry,second.exit) ;
-    }
-
-    public static final Valgen or(Valgen choice1, Valgen choice2) {
+	/**
+	 * Union entre deux automates
+	 * @param choice1 un automate qui servira de premier choix dans l'union
+	 * @param choice2 un autre automate pour le second choix
+	 * @return on prend les deux automates, et on en produit un nouveau faisant l'union entre les deux
+	 */	
+	private static final Valgen or(Valgen choice1, Valgen choice2) {
 		choice1.exit.isFinal = false ;
 		choice2.exit.isFinal = false ;
-		ValgenState entry = new ValgenState() ;
-		ValgenState exit  = new ValgenState() ;
+
+		ValgenNode entry = new ValgenNode() ;
+		ValgenNode exit  = new ValgenNode() ;
 		exit.isFinal = true ;
+
 		entry.addEmptyEdge(choice1.entry) ;
 		entry.addEmptyEdge(choice2.entry) ;
+
 		choice1.exit.addEmptyEdge(exit) ;
 		choice2.exit.addEmptyEdge(exit) ;
 		return new Valgen(entry,exit) ;
-	    }
+	}
 
-	    public static final Valgen re(Object o) {
-		if (o instanceof Valgen)
-		    return (Valgen)o ;
-		else if (o instanceof Character)
-		    return c((Character)o) ;
-		else if (o instanceof String)
-		    return fromString((String)o) ;
-		else {
-		    throw new RuntimeException("bad regexp") ;
+	/**
+	 * Sucre syntaxique, pour distinger les automates des characters et des Strings passées en paramètre
+	 * Si il s'agit d'un automate, on le retourne
+	 * Si il s'agit d'une chaine de caracteres on revoit un automate qui découpe cette chaine caracteres par caracteres entre des etats
+	 * Si il s'agit d'un caractere, on renvoit un automate qui fait la transition entre deux noeuds sur ce caractere
+	 * Sinon on renvoit une erreur car on n'a pas reconnu le terme
+	 *
+	 * @param o un Object
+	 * @return un automate
+	 * @throws RuntimeException
+	 */
+	private static final Valgen re(Object o) {
+		if (o instanceof Valgen){
+			return (Valgen)o ;
 		}
-    }
+		else if (o instanceof Character){
+			return c((Character)o) ;
+		}
+		else if (o instanceof String){
+			return fromString((String)o) ;
+		}
+		else {
+			throw new RuntimeException("bad regexp") ;
+		}
+	}
 
-    public static final Valgen o(Object... rexps) {
+	/**
+	 * On execute une operation bouclable de Valgen sur des parametres
+	 * @param id_op l'indice de l'operation, soit l'union, soit une concatenation
+	 * @param rexps Object correspondant a Valgen, ou a String ou a Character
+	 * @return l'automate de l'operation
+	 */
+	private static final Valgen loop(int id_op, Object[] rexps){
 		Valgen exp = re(rexps[0]) ;
 		for (int i = 1; i < rexps.length; i++) {
-		    exp = or(exp,re(rexps[i])) ;
+			switch(id_op){
+			case 0: // union
+				exp = or(exp, re(rexps[i]));
+				break;
+			case 1: // concat			
+				exp = concat(exp, re(rexps[i]));
+				break;
+			default: // errors
+				break;
+			}
 		}
-		return exp ;
-    }
-
-    public static final Valgen g(Object... rexps) {
-		Valgen exp = e() ;
-		for (int i = 0; i < rexps.length; i++) {
-		    exp = s(exp,re(rexps[i])) ;
-		}
-		return exp ;
-    }
-
-    public static final Valgen fromString(String str) {
-		if (str.length() == 0)
-		    return e() ;
-		else
-		    return g(re(str.charAt(0)),fromString(str.substring(1))) ;
-    }
-
-    public static void main(String[] args) {
-	Valgen pat = g(r(o("foo","bar")),"") ;
-	String[] strings =
-	    { "foo" , "bar" ,
-	      "foobar", "farboo", "boofar" , "barfoo" ,
-	      "foofoobarfooX" ,
-	      "foofoobarfoo" ,
-	    } ;
-	for (String s : strings) {
-	    System.out.println(s + "\t:\t" +pat.matches(s)) ;
+		return exp;
 	}
-    }
+
+	/**
+	 * Facade pour la repetion
+	 * On creer un automate a partir de l'objet rexps, où le final et 
+	 * @param rexps l'expression a repeter
+	 * @return l'automate Valgen
+	 */
+	public static final Valgen r(Object rexps) {
+		Valgen nfa = group(rexps);
+		nfa.exit.addEmptyEdge(nfa.entry) ;
+		nfa.entry.addEmptyEdge(nfa.exit) ;
+		return nfa;
+	}
+
+	/**
+	 * Facade pour les unions
+	 * @param rexps les expressions a relier en unions
+	 * @return l'automate  Valgen de l'union
+	 * @see Valgen#or(Valgen, Valgen) or
+	 */
+	public static final Valgen o(Object ... rexps) {
+		return loop(0, rexps);
+	}
+
+	/**
+	 * @param rexps des automates
+	 * @return un automate qui est la concatenation, dans l'ordre, des automates passés en parametre
+	 * @see Valgen#concat(Valgen, Valgen) concat
+	 */
+	public static final Valgen group(Object ... rexps) {
+		return loop(1, rexps);
+	}
+
+	/**
+	 * On découpe une chaine caractere par caractere, en groupant les etats qu'on obtient
+	 * @param str la chaine a decouper
+	 * @return l'automate qui concatene les caracteres sur des transitions
+	 */
+	private static final Valgen fromString(String str) {
+
+		if (str.length() == 0){
+			return e() ;
+		}
+		else{
+			return group(re(str.charAt(0)),fromString(str.substring(1))) ;
+		}
+	}
+
+	public static void main(String[] args) {
+		Valgen pat = group(r(o(group('f','o','o'),o("bar", "aze")))) ;
+		String[] strings =
+			{ "foo" , "bar" ,
+				"foobar", "farboo", "boofar" , "barfoo" ,
+				"foofoobarfooX" ,
+				"foofoobarfoomdr" ,
+				"foofoobarmdr",
+				"aaa",
+				"a"
+			} ;
+		for (String s : strings) {
+			System.out.println(s + "\t:\t" +pat.matches(s)) ;
+		}
+	}
 }
